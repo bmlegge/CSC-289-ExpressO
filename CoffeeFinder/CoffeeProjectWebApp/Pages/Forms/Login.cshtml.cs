924ce7ce4708e993a4ac9970b0ccf74ed3255326
Jochs1989 <43485024+Jochs1989@users.noreply.github.com>
2020-04-28 20:42:14 -0400

    test

---
+++ CoffeeFinder/CoffeeProjectWebApp/Pages/Forms/Login.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeProjectWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoffeeProjectWebApp
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public LoginInformation Login { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            // TODO Create Database Library to Save Model to Database
+
+            return RedirectToPage("/Index", new { UserName = "Coffee Finder " + Login.UserName  });
+        }
+    }
+}
---
+++ CoffeeFinder/CoffeeProjectWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CoffeeProjectWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string UserName { get; set; }
+
+        public void OnGet()
+        {
+            if(string.IsNullOrWhiteSpace(UserName))
+            {
+                UserName = "Coffee Finder";
+            }
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+}
