6e98e87b85d36ca2f6f6f989c6d491190250aa6e
Bradley Legge <leggeb9867@ad.faytechcc.edu>
2020-04-28 21:04:36 -0400

    1st upload

---
+++ ExpressO.RazorPages/ExpressO.RazorPages/Pages/Forms/Login.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpressO.RazorPages.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExpressO.Pages.Forms
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Login LoginInfo { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
