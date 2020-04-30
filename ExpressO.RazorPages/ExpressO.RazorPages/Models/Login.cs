499861f951908afe6b1108fcb79ba04c1f0770f2
Bradley Legge <leggeb9867@ad.faytechcc.edu>
2020-04-29 20:24:53 -0400

    Uploaded new project

---
+++ ExpressO.RazorPages/ExpressO.RazorPages/Models/Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpressO.RazorPages.Models
+{
+    public class Login
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Must enter a username")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Must enter a password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Must enter a password")]
+        [Compare("Login.Password", ErrorMessage = "Passwords must match!")]
+        public string PasswordConfirmation { get; set; }
+    }
+}
